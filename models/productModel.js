fa72ec845472b865ebe5869b323c79f495b3da00
Maulidan Nashuha <maulidannashuha@gmail.com>
2023-09-04 19:33:28 +0700

    refactor model

--- models/productModel.js
+++ models/productModel.js
@@ -1,10 +1,14 @@
 const db = require("../db/db")
 
+const getAllProduct = async () => {
+    return await db.select('*').from('products')
+}
+
+const getProductById = async (id) => {
+    return await db.select('*').from('products').where('id', id).first()
+}
+
 module.exports = {
-    getAllProduct: async () => {
-        return await db.select('*').from('products')
-    },
-    getProductById: async (id) => {
-        return await db.select('*').from('products').where('id', id).first()
-    }
+    getAllProduct,
+    getProductById
 }
